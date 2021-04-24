2ebf41e180cbd4a44f67393293f8b5bfeea3dee1
Max Sprang <masprang@uni-mainz.de>
2021-04-24 22:07:29 +0200

    fixed predict.py hopefully

--- src/predict.py
+++ src/predict.py
@@ -2,13 +2,15 @@ import argparse, joblib
 import numpy as np
 import pandas as pd
 
-import extra_funcs
+# import extra_funcs
 
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 
 from pandarallel import pandarallel
+
+from sgt import SGT
 
 # set up argument parsing (make sure these match those in config.yml)
 parser = argparse.ArgumentParser()
@@ -17,16 +19,18 @@ args = parser.parse_args()
 
 # READ DATA
 data = pd.read_csv(args.infile)
-
+# data = pd.read_csv("../submission/input.csv")
 # embed both protein sequences respectively
 sgt_ = SGT(kappa=5,
             lengthsensitive=False,
             mode='multiprocessing')
 
-ids = ["ab_pair_{}".format(i) for i in range(data.shape[1])]
+ids = ["ab_pair_{}".format(i) for i in range(data.shape[0])]
 data["id"] = ids
-heavy_embedding = sgt_.fit_transform(data[["id", "Hchain"]])
-light_embedding = sgt_.fit_transform(data[["id", "Lchain"]])
+data["Hchain"] = data["Hchain"].map(list)
+data["Lchain"] = data["Hchain"].map(list) 
+heavy_embedding = sgt_.fit_transform(data[["id", "Hchain"]].rename(columns={"Hchain":"sequence"}))
+light_embedding = sgt_.fit_transform(data[["id", "Lchain"]].rename(columns={"Lchain":"sequence"}))
 #input transformed shoul have 800 cols containing 400 heavy and 400 light chains.
 input_transformed = pd.concat([heavy_embedding.set_index("id"), light_embedding.set_index("id")], axis=1)
 
