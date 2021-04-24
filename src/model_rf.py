40a5da6787095cf0a7893d6d817881019083850a
Mariane Goncalves Kulik <mari.ti@gmail.com>
2021-04-24 18:13:10 +0200

    New model version.

--- src/model_rf.py
+++ src/model_rf.py
@@ -9,12 +9,14 @@ Created on Sat Apr 24 07:23:00 2021
 import pandas as pd
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+import joblib
 
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
-from sklearn import metrics
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, roc_auc_score, roc_curve, f1_score
 
 data_embeddings = pd.read_csv('embeddings_first_1000_Antibodies.csv').reset_index()
 data_embeddings['index'] = "paired_"+data_embeddings['index'].astype(str)
@@ -69,6 +71,37 @@ def get_best_model(algo, params, X_train, y_train, st=0, verb=1, cv=5, iters=100
     return mdl_search.best_estimator_
 
 
+def evaluate_model(y_pred, probs,train_predictions, train_probs):
+    baseline = {}
+    baseline['recall']=recall_score(y_test, [1 for _ in range(len(y_test))])
+    baseline['precision'] = precision_score(y_test, [1 for _ in range(len(y_test))])
+    baseline['roc'] = 0.5
+    results = {}
+    results['recall'] = recall_score(y_test, y_pred)
+    results['precision'] = precision_score(y_test, y_pred)
+    results['roc'] = roc_auc_score(y_test, probs)
+    train_results = {}
+    train_results['recall'] = recall_score(y_train, train_predictions)
+    train_results['precision'] = precision_score(y_train, train_predictions)
+    train_results['roc'] = roc_auc_score(y_train, train_probs)
+    for metric in ['recall', 'precision', 'roc']:  
+          print("{0} Baseline: {1} Test: {2}, Train: {3}".format(metric.capitalize(), 
+                                                                 round(baseline[metric], 2), 
+                                                                 round(results[metric], 2), 
+                                                                 round(train_results[metric], 2)))
+     # Calculate false positive rates and true positive rates
+    base_fpr, base_tpr, _ = roc_curve(y_test, [1 for _ in range(len(y_test))])
+    model_fpr, model_tpr, _ = roc_curve(y_test, probs)
+    plt.figure(figsize = (8, 6))
+    plt.rcParams['font.size'] = 16
+    # Plot both curves
+    plt.plot(base_fpr, base_tpr, 'b', label = 'baseline')
+    plt.plot(model_fpr, model_tpr, 'r', label = 'model')
+    plt.legend();
+    plt.xlabel('False Positive Rate');
+    plt.ylabel('True Positive Rate'); plt.title('ROC Curves');
+    plt.show();
+
 data_all_embeddings = randomize_rows(data_embeddings, seed)
 
 cols=list(data_all_embeddings.columns)[1:-1]
@@ -78,27 +111,36 @@ y=data_all_embeddings['class']  # Labels
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=seed)
 
-rf_params = {'bootstrap': [True, False],
-             'max_depth': [10, 30, 60, 90, 100, None],
-             'max_features': ['auto', 'sqrt'],
-             'min_samples_leaf': [1, 2, 4],
-             'min_samples_split': [2, 5, 10],
-             'n_estimators': [200, 400]}
+# =============================================================================
+# rf_params = {'bootstrap': [True, False],
+#              'max_depth': [10, 30, 60, 90, 100, None],
+#              'max_features': ['auto', 'sqrt'],
+#              'min_samples_leaf': [1, 2, 4],
+#              'min_samples_split': [2, 5, 10],
+#              'n_estimators': [200, 400]}
+# =============================================================================
 
 rf_params = {'bootstrap': [True, False],
-             'max_depth': [10, 90, None],
+             'max_depth': [10, 100, None],
              'max_features': ['auto', 'sqrt'],
              'min_samples_leaf': [1, 4],
              'min_samples_split': [2, 10],
-             'n_estimators': [100, 200]}
+             'n_estimators': [100, 100]}
 
 rf = RandomForestClassifier()
 best_rf_model = get_best_model(rf, rf_params, X_train, y_train)
+joblib.dump(best_rf_model, )
 
 best_rf_model.fit(X_train,y_train)
 
 y_check=best_rf_model.predict(X_train)
 y_pred=best_rf_model.predict(X_test)
 
-print("Accuracy over the Train Set:", metrics.accuracy_score(y_train, y_check))
-print("Accuracy over the Test Set:", metrics.accuracy_score(y_test, y_pred))
+y_check_proba=best_rf_model.predict_proba(X_train)[:,1]
+y_pred_proba=best_rf_model.predict_proba(X_test)[:,1]
+
+evaluate_model(y_pred, y_pred_proba, y_check, y_check_proba)
+
+    
+    
+
